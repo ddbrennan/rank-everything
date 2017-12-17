0b72567f3363f3bc2ac3193b99b9133cef81cd16
ddbrennan <ddbren@gmail.com>
2017-12-17 10:47:01 -0500

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'things/top_ten', to: 'things#top_ten'
   resources :things
@@ -8,5 +10,10 @@ Rails.application.routes.draw do
   get 'thing/:id', to: 'things#show', as: 'thing_path'
   root 'things#random'
   post 'things/random', to: 'things#vote'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 
 end
